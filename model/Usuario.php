6946ea30d5bb82a663d9948b9535fceaee899024
Rodrigo Warzak <warzak@gmail.com>
2018-09-13 20:15:56 -0300

    Ajustes

--- model/Usuario.php
+++ model/Usuario.php
@@ -15,6 +15,32 @@ class Usuario{
    public function __construct($db){
       $this->conn = $db;
    }
+
+   public function emailExists()
+   {
+      $query = "SELECT * FROM ".$this->table_name." WHERE email = ?";
+
+      $stmt = $this->conn->prepare($query);
+      $this->email=htmlspecialchars(strip_tags($this->email));
+
+      $stmt->bindParam(1, $this->email);
+      $stmt->execute();
+
+      //obtem nros de linhas
+      $num = $stmt->rowCount();
+
+      if($num > 0){
+         $row = $stmt->fetch(PDO::FETCH_ASSOC);
+         $this->id = $row['id'];
+         $this->email = $row['email'];
+         $this->senha = $row['senha'];
+         $this->nivel_acesso = $row['nivel_acesso'];
+         $this->status = $row['status'];
+
+         return true;
+      }
+      return false;
+   }
 }
 
 ?>
